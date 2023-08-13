f2f7ddb5dfc96e981c56732c96c0707cedadcb3e
yugo-ibuki <y.ibuki91@gmail.com>
2023-08-13 16:51:35 +0900

    compare Original with one from npm

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Starin, { defaultProps } from './Starin.tsx'
+import OriginalStarin from './Starin.tsx'
+import { defaultProps } from './Starin.tsx'
+import Starin from 'starin'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Starin {...defaultProps} />
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
+    >
+      <h1>Imported from npm</h1>
+      <Starin {...defaultProps} />
+    </div>
+    <hr />
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
+    >
+      <h1>Imported from original</h1>
+      <OriginalStarin {...defaultProps} />
+    </div>
   </React.StrictMode>
 )
